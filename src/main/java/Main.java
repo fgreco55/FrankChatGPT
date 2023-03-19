52778d039dcd85fb86de22b3ce365a71c501ad90
Frank Greco <fgreco@javasig.com>
2023-03-18 22:35:40 -0400

    main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Main {
+import com.lilittlecat.chatgpt.offical.ChatGPT;   // https://github.com/LiLittleCat/ChatGPT
+import org.eclipse.jetty.util.IO;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class Main {
+    private static String CONFIGFILE = "src/main/java/frank.properties";
+
+    public static void main(String[] args) throws IOException {
+
+        Properties prop = getConfigProperties(CONFIGFILE);
+        String key = prop.getProperty("chatgpi.apikey");
+        java.util.Scanner userinput;
+        ChatGPT chatGPT = new ChatGPT(key);
+
+        while (true) {
+            System.out.print("Command> ");
+            userinput = new java.util.Scanner(System.in);
+
+            if (userinput.hasNextLine()) {
+                String cmd = userinput.nextLine();
+                if (!cmd.isEmpty()) {
+                    String result = chatGPT.ask(cmd);
+                    System.out.println(result.trim());
+                }
+            }
+        }
+    }
+
+    static Properties getConfigProperties(String fname) throws IOException {
+        Properties prop = new Properties();
+        InputStream in = new FileInputStream(fname);
+
+        prop.load(in);
+
+        for (Enumeration e = prop.propertyNames(); e.hasMoreElements(); ) {
+            String key = e.nextElement().toString();
+            //System.out.println(key + " = " + prop.getProperty(key));
+        }
+        return prop;
+    }
 }
